aab3cbe39da72225ec5bf2c51df9a6dbd4f985a6
Will Lane <williamlane923@gmail.com>
2021-04-16 11:40:18 -0700

    update base files

---
+++ src/routes/about.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  let email = '<williamlane923@gmail.com>'
+</script>
+
+<h1>written and maintained by William Lane <a target="_blank" href="mailto:williamlane923@gmail.com">{email}</a></h1>
+
+<style lang="sass">
+
+</style>
